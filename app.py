1327cb1bcf3ab02c200af5a16700d6cede61f64c
Andrei Stefan <14292505+andrei8782@users.noreply.github.com>
2022-02-05 23:34:18 +0000

    Added training data

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# save this as app.py
 from flask import Flask, request
 from sklearn.model_selection import train_test_split
 
@@ -24,7 +23,7 @@ def get_model(crop: str):
     model.fit(X_train, y_train)
     return model
 
-crops = ['maize', 'test']
+crops = ['maize']
 models = {}
 
 for crop in crops:
